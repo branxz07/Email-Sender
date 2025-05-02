a554b7d47b2a8e2085b33c2f06a6c3b9565b5ffe
Brandon Avalos <37785475+branxz07@users.noreply.github.com>
2025-05-02 16:09:58 -0600

    Update test_email_sender.py

--- tests/test_email_sender.py
+++ tests/test_email_sender.py
@@ -1,11 +1,12 @@
 import os
 import pytest
-from email_sender import send_email
 from unittest.mock import patch, MagicMock
 import sys
 
 # Modify sys.path to include the src directory for module imports
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from email_sender import send_email
 
 def test_send_email_success(monkeypatch):
     monkeypatch.setenv("EMAIL_ADDRESS", "testsender@example.com")
